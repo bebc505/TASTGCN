6013cb9efff84cf2dd10bb7e8bc2db6fc8b63369
bebc505 <140138718+bebc505@users.noreply.github.com>
2025-05-23 14:24:45 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,189 +1,115 @@
+import argparse
+import logging
 import numpy as np
 import torch
 import torch.nn as nn
-
 import model
 import utils
-import argparse
-import logging
 
-args = argparse.ArgumentParser(description="TASTGCN")
-args.add_argument("--dataset", default="PEMS08",help="")
-args.add_argument("--num_input", type=int, default=12)
-args.add_argument("--num_output", type=int, default=12)
-args.add_argument("--epochs", type=int, default=50)
-args.add_argument("--batch_size", type=int, default=16)
-args.add_argument("--device", type=str, default="cuda:0")
-args.add_argument("--seed", type=int, default=7)
-args.add_argument("--max_speed", type=int, default=120)
-args.add_argument("--seq_len", type=int, default=12)
-args.add_argument("--d_model", type=int, default=12)
-args.add_argument("--d_ff", type=int, default=12)
-args.add_argument("--space_emb_dim", type=int, default=3)
-args.add_argument("--day_emb_dim", type=int, default=32)
-args.add_argument("--week_emb_dim", type=int, default=32)
-args.add_argument("--learning_rate", type=float, default=1e-3)
-args = args.parse_args()
+def parse_args():
+    parser = argparse.ArgumentParser(description="TASTGCN for Traffic Forecasting")
+    parser.add_argument("--dataset", default="PEMS08", help="Dataset name")
+    parser.add_argument("--num_input", type=int, default=12)
+    parser.add_argument("--num_output", type=int, default=12)
+    parser.add_argument("--epochs", type=int, default=50)
+    parser.add_argument("--batch_size", type=int, default=16)
+    parser.add_argument("--device", type=str, default="cuda:0")
+    parser.add_argument("--seed", type=int, default=7)
+    parser.add_argument("--max_speed", type=int, default=120)
+    parser.add_argument("--seq_len", type=int, default=12)
+    parser.add_argument("--d_model", type=int, default=12)
+    parser.add_argument("--d_ff", type=int, default=12)
+    parser.add_argument("--space_emb_dim", type=int, default=3)
+    parser.add_argument("--day_emb_dim", type=int, default=32)
+    parser.add_argument("--week_emb_dim", type=int, default=32)
+    parser.add_argument("--learning_rate", type=float, default=1e-3)
+    return parser.parse_args()
 
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(message)s'
-)
-log = logging.getLogger(__name__)
+def setup_logging():
+    logging.basicConfig(level=logging.INFO, format="%(message)s")
+    return logging.getLogger(__name__)
 
-def train(training_input, training_target, batch_size):
-    permutation = torch.randperm(training_input.shape[0])
-    epoch_training_losses = []
-    for i in range(0, training_input.shape[0], batch_size):
-        net.train()
-        optimizer.zero_grad()
+def train(model, optimizer, criterion, train_input, train_target, A, S, V, batch_size, device, means, stds):
+    model.train()
+    losses, maes, rmses, mapes = [], [], [], []
+    permutation = torch.randperm(train_input.shape[0])
+    for i in range(0, train_input.shape[0], batch_size):
         indices = permutation[i:i + batch_size]
-
-        X_batch, Y_batch = training_input[indices], training_target[indices]
-
-        X_batch = X_batch.to(device=args.device)
-        Y_batch = Y_batch.to(device=args.device)
-        out = net(X_batch,A,S,V)
-        loss = loss_criterion(out, Y_batch)
+        X_batch = train_input[indices].to(device)
+        Y_batch = train_target[indices].to(device)
+        optimizer.zero_grad()
+        outputs = model(X_batch, A, S, V)
+        loss = criterion(outputs, Y_batch)
         loss.backward()
         optimizer.step()
-        epoch_training_losses.append(loss.detach().cpu().numpy())
-    return sum(epoch_training_losses)/len(epoch_training_losses)
-
-def validate(val_input, val_target):
-    net.eval()
-    temp_validation_losses = []
-    temp_validation_maes = []
-    temp_validation_rmses = []
-    temp_validation_mapes = []
-    val_input = val_input.to(device=args.device)
-    val_target = val_target.to(device=args.device)
-    permutation = torch.randperm(val_input.shape[0])
-    for i in range(0, val_input.shape[0], args.batch_size):
-        indices = permutation[i:i + args.batch_size]
-        X_batch_val, Y_batch_val = val_input[indices], val_target[indices]
-        X_batch_val = X_batch_val.to(device=args.device)
-        Y_batch_val = Y_batch_val.to(device=args.device)
-        output = net(X_batch_val, A,  S, V)
-        val_loss = loss_criterion(output, Y_batch_val).to(device="cpu")
-        temp_validation_losses.append(val_loss.detach().numpy().item())
-
-        out_unnormalized = output.detach().cpu().numpy() * stds[0] + means[0]
-        target_unnormalized = Y_batch_val.detach().cpu().numpy() * stds[0] + means[0]
-        mae = np.mean(np.absolute(out_unnormalized - target_unnormalized))
-        temp_validation_maes.append(mae)
-        mse = np.mean((out_unnormalized - target_unnormalized) ** 2) 
-        rmse = np.sqrt(mse) 
-        temp_validation_rmses.append(rmse) 
-
-        epsilon = 1e-5
-        abs_error = np.abs(out_unnormalized - target_unnormalized)
-        denominator = np.abs(target_unnormalized) + epsilon
-        mape = abs_error / denominator
-
-        valid_mask = (~np.isnan(mape)) & (~np.isinf(mape)) & (mape <= 5)
-        if np.any(valid_mask):
-            mape = np.mean(mape[valid_mask]) * 100
-        else:
-            mape = 0.0
-        temp_validation_mapes.append(mape)
+        losses.append(loss.item())
+        out = outputs.detach().cpu().numpy() * stds[0] + means[0]
+        target = Y_batch.detach().cpu().numpy() * stds[0] + means[0]
+        mae, rmse, mape = utils.compute_metrics(out, target)
+        maes.append(mae)
+        rmses.append(rmse)
+        mapes.append(mape)
+    return np.mean(losses), np.mean(maes), np.mean(rmses), np.mean(mapes)
 
 
-    return temp_validation_losses,temp_validation_maes,temp_validation_rmses,temp_validation_mapes
+def evaluate(model, input_data, target_data, means, stds, batch_size, device, A, S, V):
+    model.eval()
+    losses, maes, rmses, mapes = [], [], [], []
+    criterion = nn.L1Loss().to(device)
+    permutation = torch.randperm(input_data.shape[0])
+    for i in range(0, input_data.shape[0], batch_size):
+        indices = permutation[i:i + batch_size]
+        X_batch = input_data[indices].to(device)
+        Y_batch = target_data[indices].to(device)
+        outputs = model(X_batch, A, S, V)
+        loss = criterion(outputs, Y_batch).item()
+        losses.append(loss)
+        out = outputs.detach().cpu().numpy() * stds[0] + means[0]
+        target = Y_batch.detach().cpu().numpy() * stds[0] + means[0]
+        mae, rmse, mape = utils.compute_metrics(out, target)
+        maes.append(mae)
+        rmses.append(rmse)
+        mapes.append(mape)
+    return np.mean(losses), np.mean(maes), np.mean(rmses), np.mean(mapes)
 
-def test(test_input, test_target):
-    net.eval()
-    test_input = test_input.to(device=args.device)
-    test_target = test_target.to(device=args.device)
-    test_maes = []
-    test_rmses = []
-    test_mapes = []
-    permutation = torch.randperm(test_input.shape[0])
-    for i in range(0, test_input.shape[0], args.batch_size):
-        indices = permutation[i:i + args.batch_size]
-        X_batch_test, Y_batch_test = test_input[indices], test_target[indices]
-        X_batch_test = X_batch_test.to(device=args.device)
-        Y_batch_test = Y_batch_test.to(device=args.device)
-        output = net(X_batch_test, A, S, V)
 
-        out_unnormalized = output.detach().cpu().numpy() * stds[0] + means[0]
-        target_unnormalized = Y_batch_test.detach().cpu().numpy() * stds[0] + means[0]
-        mae = np.mean(np.absolute(out_unnormalized - target_unnormalized))
-        test_maes.append(mae)
-        mse = np.mean((out_unnormalized - target_unnormalized) ** 2)
-        rmse = np.sqrt(mse)
-        test_rmses.append(rmse)
-        epsilon = 1e-5
-        abs_error = np.abs(out_unnormalized - target_unnormalized)
-        denominator = np.abs(target_unnormalized) + epsilon
-        mape = abs_error / denominator
-        test_mask = (~np.isnan(mape)) & (~np.isinf(mape)) & (mape <= 5)
-        if np.any(test_mask):
-            mape = np.mean(mape[test_mask]) * 100
-        else:
-            mape = 0.0
-        test_mapes.append(mape)
-    return test_maes,test_rmses,test_mapes
-
-if __name__ == '__main__':
+def main():
+    args = parse_args()
+    log = setup_logging()
     torch.manual_seed(args.seed)
     A, X, V, S, means, stds = utils.load_data(args)
     X = utils.time_index_emb(X)
-    split_line1 = int(X.shape[2] * 0.6)
-    split_line2 = int(X.shape[2] * 0.8)
-    train_original_data = X[:, :, :split_line1]
-    val_original_data = X[:, :, split_line1:split_line2]
-    test_original_data = X[:, :, split_line2:]
-    training_input, training_target = utils.generate_dataset(train_original_data,
-                                                             num_timesteps_input=args.num_input,
-                                                             num_timesteps_output=args.num_output)
-    val_input, val_target = utils.generate_dataset(val_original_data,
-                                                   num_timesteps_input=args.num_input,
-                                                   num_timesteps_output=args.num_output)
-    test_input, test_target = utils.generate_dataset(test_original_data,
-                                                     num_timesteps_input=args.num_input,
-                                                     num_timesteps_output=args.num_output)
-    net = model.TASTGCN(args,A.size(0))
-    net = net.to(device=args.device)
-    optimizer = torch.optim.Adam(net.parameters(), lr=args.learning_rate)
-    loss_criterion = nn.L1Loss()
-    loss_criterion = loss_criterion.to(device=args.device)
-
-    training_losses = []
-    validation_losses = []
-    validation_maes = []
-    validation_rmses = []
-    validation_mapes = []
-    best_test_mae = 999
-    best_test_rmse = 999
-    best_test_mape = 999
+    # Split dataset
+    total_len = X.shape[2]
+    train_len = int(total_len * 0.6)
+    val_len = int(total_len * 0.8)
+    train_data = X[:, :, :train_len]
+    val_data = X[:, :, train_len:val_len]
+    test_data = X[:, :, val_len:]
+    train_input, train_target = utils.generate_dataset(train_data, args.num_input, args.num_output)
+    val_input, val_target = utils.generate_dataset(val_data, args.num_input, args.num_output)
+    test_input, test_target = utils.generate_dataset(test_data, args.num_input, args.num_output)
+    model_instance = model.TASTGCN(args, A.size(0)).to(args.device)
+    optimizer = torch.optim.Adam(model_instance.parameters(), lr=args.learning_rate)
+    criterion = nn.L1Loss().to(args.device)
+    best_mae, best_rmse, best_mape = float("inf"), float("inf"), float("inf")
     for epoch in range(args.epochs):
-        loss= train(training_input, training_target,batch_size =  args.batch_size)
-        training_losses.append(loss)
-        with torch.no_grad():
-            temp_validation_losses,temp_validation_maes,temp_validation_rmses,temp_validation_mapes = validate(val_input,val_target)
-            out = None
-        avg_val_loss = np.mean(temp_validation_losses)
-        avg_val_mae = np.mean(temp_validation_maes)
-        avg_val_rmse = np.mean(temp_validation_rmses)
-        avg_val_mape = np.mean(temp_validation_mapes)
+        train_loss, train_mae, train_rmse, train_mape = train(
+            model_instance, optimizer, criterion, train_input, train_target,
+            A, S, V, args.batch_size, args.device, means, stds
+        )
+        val_loss, val_mae, val_rmse, val_mape = evaluate(
+            model_instance, val_input, val_target, means, stds, args.batch_size, args.device, A, S, V
+        )
+        test_loss, test_mae, test_rmse, test_mape = evaluate(
+            model_instance, test_input, test_target, means, stds, args.batch_size, args.device, A, S, V
+        )
+        log.info(f"Epoch {epoch + 1}:")
+        log.info(f"  Train      -> MAE: {train_mae:.4f}, RMSE: {train_rmse:.4f}, MAPE: {train_mape:.2f}%")
+        log.info(f"  Validation -> MAE: {val_mae:.4f}, RMSE: {val_rmse:.4f}, MAPE: {val_mape:.2f}%")
+        if test_mae < best_mae:
+            best_mae, best_rmse, best_mape = test_mae, test_rmse, test_mape
+    log.info(f"Best Test Result     -> MAE: {best_mae:.4f}, RMSE: {best_rmse:.4f}, MAPE: {best_mape:.2f}%")
 
-        validation_losses.append(avg_val_loss)
-        validation_maes.append(avg_val_mae)
-        validation_rmses.append(avg_val_rmse)
-        validation_mapes.append(avg_val_mape)
-
-        log.info(f"Epoch {epoch + 1} : Validation MAE: {validation_maes[-1]:.4f}, Validation RMSE: {validation_rmses[-1]:.4f}, "
-                 f"Validation MAPE: {validation_mapes[-1]:.4f}")
-
-        with torch.no_grad():
-            test_maes, test_rmses, test_mapes = test(test_input, test_target)
-            avg_test_mae = np.mean(test_maes)
-            avg_test_rmse = np.mean(test_rmses)
-            avg_test_mape = np.mean(test_mapes)
-            log.info(f"Test MAE: {avg_test_mae:.4f}, Test RMSE: {avg_test_rmse:.4f}, Test MAPE: {avg_test_mape:.2f}%")
-            if(avg_test_mae<best_test_mae):
-                best_test_mae = avg_test_mae
-                best_test_rmse = avg_test_rmse
-                best_test_mape = avg_test_mape
-    log.info(f"Best test result: MAE: {best_test_mae:.4f}, RMSE: {best_test_rmse:.4f}, MAPE: {best_test_mape:.2f}%")
+if __name__ == "__main__":
+    main()
